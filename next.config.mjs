34fcaf88d52b072cc5f662a6eba36317ba727307
Jocelyn-MB <mancillablancasjocelyn@gmail.com>
2025-11-03 05:50:37 -0600

    Fix build: add types and update next.config for PWA

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import withPWA from "next-pwa";
 const nextConfig = {
   // Otras configuraciones de Next.js van aquí (ej. images, i18n, etc.)
   reactStrictMode: true,
+
+  typescript: {
+    ignoreBuildErrors: false, // cámbialo a true solo si sigue fallando
+  },
+
+
   turbopack: {}, // Habilitar Turbopack (experimental)
   images: {
     remotePatterns: [
@@ -16,6 +22,7 @@ const pwaConfig = withPWA({
   dest: 'public', // Directorio donde se generará el Service Worker
   register: true, // Registra automáticamente el Service Worker
   skipWaiting: true, // Forzar la activación del nuevo Service Worker
+  disable: process.env.NODE_ENV === "development", // Desactiva PWA en modo dev
  
 });
 
